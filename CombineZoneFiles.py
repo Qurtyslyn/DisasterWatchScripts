381fa09ebb7d4f2c886f005752cceedb43e30520
Curtis Blackley <qurtys.lyn@gmail.com>
2026-06-29 13:24:25 -0600

    Added County Zones to Zones file

--- CombineZoneFiles.py
+++ CombineZoneFiles.py
@@ -9,24 +9,40 @@ with open(zonesJSON, 'r') as file:
 #New Zones Files
 countyZones = './CountyZones.json'
 fireZones = './FireZones.json'
+countyIDs = 'CountyZoneIDs.json'
 
-# with open(countyZones, 'r') as file:
-#     countyData = json.load(file)
 
-# for feature in countyData['features']:
-#     feature['properties']['ID'] = feature['properties']['STATE'] + 'C' + feature['properties']
-#     print(feature['properties'])
-#     break
-print(len(zonesData['features']))
+with open(countyZones, 'r') as file:
+    countyData = json.load(file)
 
-with open(fireZones, 'r') as file:
-    fireData = json.load(file)
+with open(countyIDs, 'r') as file:
+    countyIDData = json.load(file)
 
-for feature in fireData['features']:
-    feature['properties']['ID'] = feature['properties']['STATE'] + 'Z' + feature['properties']['ZONE']
+#print(len(zonesData['features']))
+for feature in countyData['features']:
+    #feature['properties']['ID'] = feature['properties']['STATE'] + 'C' + feature['properties']
+    
+
+    data = next((item for item in countyIDData['features'] if (item['properties']['name'] == feature['properties']['COUNTYNAME'] and item['properties']['state'] == feature['properties']['STATE'] )), None)
+    
+    if data == None:
+        continue
+
+    feature['properties']['ID'] = data['properties']['id']
+    
+
     zonesData['features'].append(feature)
 
-print(len(zonesData['features']))
+#print(len(zonesData['features']))
+
+# with open(fireZones, 'r') as file:
+#     fireData = json.load(file)
+
+# for feature in fireData['features']:
+#     feature['properties']['ID'] = feature['properties']['STATE'] + 'Z' + feature['properties']['ZONE']
+#     zonesData['features'].append(feature)
+
+# print(len(zonesData['features']))
 
 with open(zonesJSON, 'w') as file:
     json.dump(zonesData, file)
