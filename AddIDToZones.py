c96364951098f75a0669cfae5760234b12f4bba1
Curtis Blackley <qurtys.lyn@gmail.com>
2026-06-29 10:05:19 -0600

    Added missing Z to AddIDToZones

--- AddIDToZones.py
+++ AddIDToZones.py
@@ -1,13 +1,15 @@
 import json
 
-zonesJSON = "./NWSZones.geojson"
+zonesJSON = "./Zones.geojson"
 
 with open(zonesJSON, 'r') as file:
     zonesData = json.load(file)
 
 #Rename Name in Zones file to ID to match coastal (Which already has Name and ID)
 for feature in zonesData['features']:
-    feature['properties']['ID'] = feature['properties']['STATE_ZONE']
+    if 'STATE_ZONE' in feature['properties'].keys():
+        newZone = feature['properties']['STATE'] + 'Z' + feature['properties']['ZONE']
+        feature['properties']['ID'] = newZone
     
 with open(zonesJSON, 'w') as file:
     json.dump(zonesData, file)
